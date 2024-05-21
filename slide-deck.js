oddbird/slide-deck

commit 7cfbbcb6a5e27dfb6f49fd92956fa4a39dadec32

Update slide-deck.js
Co-authored-by: James Stuckey Weber <james@oddbird.net>

slide-deck.js
@@ -617,7 +617,7 @@ class slideDeck extends HTMLElement {
     }
   }
 
-  #isPrivateKeydown = (event) => {
+        const type = event.target.getAttribute('type')?.toLowerCase();
     // it's only private if the focus is somewhere else
     if (event.target === this.#body) { return; }
 

commit 9f663ca16eb63ddc0e155ce932249c1b02abd8aa

Fix a typo introduced by 7cfbbcb

slide-deck.js
@@ -617,7 +617,7 @@ class slideDeck extends HTMLElement {
     }
   }
 
-        const type = event.target.getAttribute('type')?.toLowerCase();
+  #isPrivateKeydown = (event) => {
     // it's only private if the focus is somewhere else
     if (event.target === this.#body) { return; }
 